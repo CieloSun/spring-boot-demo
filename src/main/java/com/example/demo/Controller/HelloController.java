77448de6af2a4146b8546365f599b860cab819f7
CieloSun <632898354@qq.com>
2017-08-09 10:34:18 +0800

    delete page

--- src/main/java/com/example/demo/Controller/HelloController.java
+++ src/main/java/com/example/demo/Controller/HelloController.java
@@ -1,13 +1,20 @@
 package com.example.demo.Controller;
 
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
 public class HelloController {
-    @RequestMapping
+    @GetMapping("hello")
     public String index(){
         return "Hello World";
+    }
+    @GetMapping
+    public String page(ModelMap modelMap){
+        modelMap.addAttribute("title","Cielo's Home");
+        return "index";
     }
 }
